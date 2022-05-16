57235dd358b02d36cde219755c935f3382b3629a
thaianealm <cont.thaiane@gmail.com>
2022-05-16 17:09:52 -0300

    Add a new task

--- to-do-list/assets/js/script.js
+++ to-do-list/assets/js/script.js
@@ -1,5 +1,19 @@
+//validate input field
 document.querySelector('#push').onclick = function(){
-    if(document.querySelector('.newtask input').value.length == 0){
+    if(document.querySelector('#newtask input').value.length == 0){
         alert("Please Enter a Task");
+    }
+    //Add a new task
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                <img src="assets/img/recycle-bin.png" alt="">
+                </button>
+            </div>
+        `;
     }
 }
