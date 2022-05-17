e28f1da10a9156fdbc3c0fa7e800bcd0d4b82687
thaianealm <cont.thaiane@gmail.com>
2022-05-17 10:27:29 -0300

    crossing out a completed task

--- to-do-list/assets/js/script.js
+++ to-do-list/assets/js/script.js
@@ -1,4 +1,4 @@
-//validate input field
+//Validate input field
 document.querySelector('#push').onclick = function(){
     if(document.querySelector('#newtask input').value.length == 0){
         alert("Please Enter a Task");
@@ -21,6 +21,14 @@ document.querySelector('#push').onclick = function(){
         for(var i=0; i<current_tasks.length; i++){
             current_tasks[i].onclick = function(){
                 this.parentNode.remove();
+            }
+        }
+
+        //Crossing out a completed task
+        var tasks = document.querySelectorAll(".task");
+        for(var i = 0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
             }
         }
     }
