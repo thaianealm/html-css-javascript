9d80da4ef71fd10b5efb0d987a298185f431dc20
thaianealm <cont.thaiane@gmail.com>
2022-05-30 14:13:22 -0300

    first commit: change background

---
+++ change-background/assets/js/script.js
@@ -0,0 +1,15 @@
+document.getElementById('bnt-strawberry').addEventListener('click', function(){
+    document.querySelector('body').setAttribute('class','strawberry');
+});
+
+document.getElementById('bnt-cherry').addEventListener('click', function(){
+    document.querySelector('body').setAttribute('class','cherry');
+});
+
+document.getElementById('bnt-orange').addEventListener('click', function(){
+    document.querySelector('body').setAttribute('class','orange');
+});
+
+document.getElementById('bnt-banana').addEventListener('click', function(){
+    document.querySelector('body').setAttribute('class', 'banana');
+});
