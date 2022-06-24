01023b34a5690d1b04a7c9052ac9699263f964d7
thaianealm <cont.thaiane@gmail.com>
2022-06-24 13:40:17 -0300

    js-correction: month

--- mini-calendar/assets/js/script.js
+++ mini-calendar/assets/js/script.js
@@ -4,7 +4,7 @@ let date = new Date();
 let dayNumber = date.getDate();
 let month = date.getMonth();
 let dayName = date.toLocaleString(lang, {weekday: 'long'});
-let monthName = date.toLocaleString(lang, {weekday: 'long'});
+let monthName = date.toLocaleString(lang, {month: 'long'});
 let year = date.getFullYear();
 
 
